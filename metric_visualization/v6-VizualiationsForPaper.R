821480ba74ada4c4895996caa4d4437f44a77b4f
Jeremy Block <j.block@ufl.edu>
2023-06-22 16:29:03 -0400

    Adding visualization for Factuality

--- metric_visualization/v6-VizualiationsForPaper.R
+++ metric_visualization/v6-VizualiationsForPaper.R
@@ -77,3 +77,47 @@ gatheredMetrics2 %>%
   labs(title = "Accuracy Measures by Audience and Example")
 ggsave(paste0(folder,"accuracyMeasures.png"))
 
+
+#Stacked bar chart for Factuality Errors
+
+# Create the data frame
+factuality <- data.frame(
+  Participant = c("Session 1", "Session 2", "Session 3"),
+  "Other Errors" = c(7.39, 2.86, 6.06),
+  "Content Verifiability Errors" = c(1.48, 6.67, 6.06),
+  "Discourse Errors" = c(0.00, 0.95, 1.01),
+  "Semantic Frame Errors" = c(0.00, 0.00, 0.00)
+  )#, No_Errors = c(91.13, 89.52, 86.87))
+
+factuality$Participant <- factor(factuality$Participant, levels = rev(factuality$Participant))
+
+# Define the desired order of variables
+variable_order <- rev(c( "Other.Errors", "Content.Verifiability.Errors", "Discourse.Errors", "Semantic.Frame.Errors")) #"No Errors",
+
+# Reorder the columns based on the desired order
+# factuality <- factuality[, c("Participant", variable_order)]
+
+
+# Melt the data frame to long format
+data_long <- reshape2::melt(factuality, id.vars = "Participant") %>%
+  mutate("Error Type" = variable)
+
+# Reorder the levels of the "Error Type" column
+# data_long$`Error Type` <- factor(data_long$`Error Type`, levels = variable_order)
+
+
+# Create the stacked bar charts
+ggplot(data_long, aes(x = value, y = Participant, fill = `Error Type`)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Factuality Errors by Participant Summary",
+       x = "Percentage of Factuality Errors Present in Baseline Summary",
+       y = NULL) +
+  scale_fill_manual(values = c("#570047", "#a8194b", "#e55838", "#ffa600"), #c("#FF0000", "#00FF00", "#0000FF", "#FFFF00")) + #, "#FFFFFF")) +
+                    # breaks = variable_order,
+                    # labels = variable_order)+
+  )+
+  xlim(0, 100)+
+  theme_minimal()+
+  theme(legend.position = c(0.75, 0.5))
+ggsave(paste0(folder,"factuality.png"), width = 6, height = 3 )
+ 
