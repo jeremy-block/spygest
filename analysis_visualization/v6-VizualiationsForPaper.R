fd91cccb58794a9b14871e2e0803e2c57d298341
Jeremy Block <j.block@ufl.edu>
2023-10-09 16:27:18 -0400

    Clean up of accuracy visualization.

--- analysis_visualization/v6-VizualiationsForPaper.R
+++ analysis_visualization/v6-VizualiationsForPaper.R
@@ -9,7 +9,6 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-# install.packages("readxl")
 library(readxl)
 
 
@@ -21,8 +20,8 @@ data$example <- factor(data$example)
 data$person <- factor(data$person)
 data$ground_truth <- factor(data$ground_truth)
 
-levels(data$audience) <- c("No Aud.", "Self", "Peer", "Manager")
-levels(data$example) <- c("No Exp.", "Manual", "Man.Masked", "Temp.Masked")
+levels(data$audience) <- c("No Audience", "Self", "Peer", "Manager")
+levels(data$example) <- c("0-Shot (none)", "1-Shot (manual)", "Masked Manual", "Masked Template")
 levels(data$person) <- c("P1", "P2", "P3")
 levels(data$ground_truth) <- c("Manual", "Baseline", "Additional")
 
@@ -38,7 +37,7 @@ gathered_metrics <- data %>%
   gather(key = "metric", value = "Score", rouge, bleu, bleurt, ter) %>%
   mutate(Audience = audience) %>%
   select(-audience) %>%
-  mutate(Example = example) %>%
+  mutate(Prompting = example) %>%
   select(-example) %>%
   select(-word_count, -character_count)
 
@@ -52,7 +51,7 @@ gathered_metrics %>%
   labs(title = "Accuracy Measures by Audience")
 
 gathered_metrics %>%
-  ggplot(aes(x = ground_truth, y = Score, fill = Example)) +
+  ggplot(aes(x = ground_truth, y = Score, fill = Prompting)) +
   geom_boxplot() +
   facet_wrap(~metric, scales = "free") +
   labs(title = "Accuracy Measures by Example")
@@ -61,22 +60,22 @@ gathered_metrics %>%
 gathered_metrics %>%
   ggplot(aes(x = ground_truth, y = Score, fill = Audience)) +
   geom_boxplot() +
-  facet_grid(metric ~ Example, scales = "free") +
+  facet_grid(metric ~ Prompting, scales = "free") +
   labs(title = "Accuracy Measures by Audience and Example")
 
 # Put both Accuracy and Example in the same figure
 gathered_metrics2 <- gathered_metrics %>%
-  gather(key = "key_factor", value = "Level", Audience, Example)
+  gather(key = "key_factor", value = "Experiment Level", Audience, Prompting)
 # Reorder the levels of the 'category' factor
 
-levels(gathered_metrics2$Level) <- c("No Aud.", "Self", "Peer", "Manager", "No Exp.", "Manual", "Man.Masked", "Temp.Masked")
-gathered_metrics2$Level <- factor(gathered_metrics2$Level, levels = c("No Aud.", "Self", "Peer", "Manager", "No Exp.", "Manual", "Man.Masked", "Temp.Masked"))
+levels(gathered_metrics2$`Experiment Level`) <- c("No Audience", "Self", "Peer", "Manager", "0-Shot (none)", "1-Shot (manual)", "Masked Manual", "Masked Template")
+gathered_metrics2$`Experiment Level` <- factor(gathered_metrics2$`Experiment Level`, levels = c("No Audience", "Self", "Peer", "Manager", "0-Shot (none)", "1-Shot (manual)", "Masked Manual", "Masked Template"))
 gathered_metrics2 %>%
-  ggplot(aes(x = ground_truth, y = Score, fill = Level)) +
+  ggplot(aes(x = ground_truth, y = Score, fill = `Experiment Level`)) +
   geom_boxplot(color = "#888888") +
   facet_wrap(key_factor ~ metric, nrow = 2, ncol = 4, scales = "free") +
   scale_fill_manual(values = c("#ffffcc", "#c2e699", "#78c679", "#238443", "#feebe2", "#fbb4b9", "#f768a1", "#ae017e")) +
-  labs(title = "Accuracy Measures by Audience and Example", x = "Ground Truth Comparison")
+  labs(title = "Accuracy Measures by Audience Type and Prompt Engineering Approach", x = "Ground Truth Comparison")
 ggsave(paste0(folder, "accuracyMeasures.png"), height = 10, width = 22)
 ggsave(paste0(folder, "accuracyMeasures.pdf"), height = 10, width = 22)
 
@@ -87,11 +86,17 @@ ggsave(paste0(folder, "accuracyMeasures.pdf"), height = 10, width = 22)
 
 factuality <- read_excel("data/calculatingFactuality.xlsx", sheet = 1)
 # check the imported data shape
-print(factuality)
+# print(factuality)
 
 factuality <- factuality %>%
   mutate(`Repeated Phrase` = `Other Errors`) %>%
-  select(`Participant Number`, `Repeated Phrase`, `Content Verifiability Errors`, `Discourse Errors`, `Semantic Frame Errors`)
+  select(
+    `Participant Number`,
+    `Repeated Phrase`,
+    `Content Verifiability Errors`,
+    `Discourse Errors`,
+    `Semantic Frame Errors`
+  )
 
 # Convert the "Participant Number" column to a factor with custom levels and labels
 # Also modify ordering for plotting.
@@ -106,7 +111,9 @@ factuality <- factuality %>%
 fact_long <- reshape2::melt(factuality, id.vars = "participant") %>%
   mutate("Error Type" = variable)
 
-print(fact_long)
+# check the data shape is long now
+# print(fact_long)
+
 # Create the stacked bar charts
 ggplot(fact_long, aes(x = value, y = participant, fill = `Error Type`)) +
   geom_bar(stat = "identity") +
